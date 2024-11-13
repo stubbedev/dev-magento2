d89861249c93d771c45f44d7d7f690063ef51acf
Alexander Stage <abs@stubbe.dev>
2024-11-13 19:59:02 +0100

    improved update wizard

--- update.sh
+++ update.sh
@@ -11,4 +11,23 @@ for CF in "${COMPOSE_FILES[@]}"; do
   fi
 done
 
+read -rn 1 -p "Would you like to added 'local.magento' to your hosts file as an alias for localhost? [Y]es/(n)o." INSTALL_HOSTS_ALIAS
+case "$REPLY" in
+  Y | y | yes | Yes)
+    sudo grep -qxF '127.0.0.1       local.magento' /etc/hosts || sudo echo '127.0.0.1       local.magento' >> /etc/hosts
+    ;;
+  *)
+    echo "Skipping adding alias!"
+    ;;
+esac
+
+VERSION_DIRS=(./versions/*)
+for VD in "${VERSION_DIRS[@]}"; do
+  if [ -d "$VD" ]; then
+    cp "./init.sh" "$VD"
+  fi
+done
+
+echo "cd into ./versions/VERSION you wish to run"
+echo "then simply run sudo ./init.sh"
 
