f5ba8cb313d8e154e701cdf097f8d00411a7c674
Alexander Stage <abs@stubbe.dev>
2024-11-13 20:56:02 +0100

    more messaging

--- init.sh
+++ init.sh
@@ -8,8 +8,12 @@ function install_magento_data {
   sleep 60
   CONTAINER_NAME=$(sudo docker ps | awk '{print $NF}' | grep -w web)
   local "$CONTAINER_NAME"
-  sudo docker exec -it "$CONTAINER_NAME" install-magento
-  sudo docker exec -it "$CONTAINER_NAME" install-sampledata
+  sudo docker exec -it $CONTAINER_NAME install-magento
+  sudo docker exec -it $CONTAINER_NAME install-sampledata
+
+  echo "You can now visit the site:"
+  echo "frontend: http://local.magento"
+  echo "backend: http://local.magento/admin"
 }
 
 install_magento_data
