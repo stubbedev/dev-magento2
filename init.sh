6a14c15616a43ef18d7775dd10183d8ba681511b
Alexander Stage <abs@stubbe.dev>
2024-11-13 21:08:14 +0100

    improve init sh

--- init.sh
+++ init.sh
@@ -2,18 +2,24 @@
 
 sudo docker compose up -d
 function install_magento_data {
-  echo "======================================"
-  echo "|          Waiting 60s               |"
-  echo "======================================"
-  sleep 60
-  CONTAINER_NAME=$(sudo docker ps | awk '{print $NF}' | grep -w web)
-  local "$CONTAINER_NAME"
-  sudo docker exec -it $CONTAINER_NAME install-magento
-  sudo docker exec -it $CONTAINER_NAME install-sampledata
+  message="Waiting "
+  unit=" seconds for container to warm up."
+	secs=$((60))
+	while [ $secs -gt 0 ]; do
+		echo -ne "$message$secs$unit\033[0K\r"
+		sleep 1
+		: $((secs--))
+	done
+	CONTAINER_NAME=$(sudo docker ps | awk '{print $NF}' | grep -w web)
+	local "$CONTAINER_NAME"
+	sudo docker exec -it $CONTAINER_NAME install-magento
+	sudo docker exec -it $CONTAINER_NAME install-sampledata
 
-  echo "You can now visit the site:"
-  echo "frontend: http://local.magento"
-  echo "backend: http://local.magento/admin"
+	echo "You can now visit the site:"
+	echo "frontend: http://local.magento"
+	echo "backend: http://local.magento/admin"
+  echo "credentials:"
+  cat ./env
 }
 
 install_magento_data
